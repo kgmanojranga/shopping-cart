ee70ab87d238a83b2767f81fe4d7a6d1fb8ee5b1
Manoj Ranga <manojranga7583@gmail.com>
2023-08-23 16:19:07 +0530

    home modification

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,38 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100vh",
+        justifyContent: "start",
+        background: "white",
+        position: "relative",
+        // top: "5rem",
+        alignItems: "center"
+      }}
+    >
+      <h1
+        style={{
+          textAlign: "center",
+          background: "white",
+          marginBottom: "0",
+          padding: "2rem"
+        }}
+      >
+        Manoj's Shopping Cart
+      </h1>
+      <img
+        src="/public/imgs/shopping-cart.jpg"
+        style={{
+          display: "block",
+          width: "50rem",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "3rem"
+        }}
+      />
+    </div>
+  );
 }
